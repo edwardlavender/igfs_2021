93ba5402731b637203870c87336b3a2a116863ac
Edward Lavender <el72@st-andrews.ac.uk>
2021-11-30 20:58:29 +0000

    Map species richness by haul in analyse_fish_catches.R.

--- R/analyse_fish_catches.R
+++ R/analyse_fish_catches.R
@@ -16,6 +16,7 @@
 
 #### Wipe workspace
 rm(list = ls())
+source("./R/define_global_param.R")
 
 #### Essential packages
 library(magrittr)
@@ -23,6 +24,8 @@ library(prettyGraphics)
 
 #### Load data
 catches <- readRDS("./data/fish/catches.rds")
+coast   <- readRDS("./data/spatial/coast/coast_s.rds")
+bathy   <- raster::raster("./data/spatial/bathy/bathy.tif")
 
 
 ################################
@@ -30,15 +33,61 @@ catches <- readRDS("./data/fish/catches.rds")
 #### Species richness
 
 #### Summarise the number of species per haul
-catches %>%
+spp_per_haul <-
+  catches %>%
   dplyr::group_by(Haul) %>%
-  dplyr::summarise(n_spp = length(unique(Species))) %>%
+  dplyr::mutate(n_spp = length(unique(Species))) %>%
+  dplyr::slice(1L) %>%
+  dplyr::ungroup()
+spp_per_haul %>%
   dplyr::pull(n_spp) %>%
   utils.add::basic_stats()
+# min  mean median max   sd IQR  MAD
+#  17 29.24   28.5  48 6.52 9.5 7.41
+
 
 #### Map the number of species per haul
+## Set up plot to save
+png("./fig/map_n_spp_per_haul.png", height = 10, width = 10, units = "in", res = 600)
+## Define colour scheme
+spp_per_haul_zlim <- c(15, 50)
+spp_per_haul_col_param <- pretty_cols_brewer(zlim = spp_per_haul_zlim)
+spp_per_haul$col <- spp_per_haul_col_param$col[findInterval(spp_per_haul$n_spp, spp_per_haul_col_param$breaks)]
+spp_per_haul_paa <- pretty_axis(side = 4,
+                                lim = list(range(spp_per_haul_col_param$breaks)),
+                                control_axis = list(las = TRUE),
+                                add = FALSE)
+## Base map
+pretty_map(add_rasters = list(x = bathy,
+                              plot_method = raster::plot,
+                              col = bathy_col_param$col,
+                              legend = FALSE),
+           add_polys = list(x = coast, col = "grey"),
+           add_points = list(x = spp_per_haul$Lon, y = spp_per_haul$Lat,
+                             pch = 21,
+                             cex = spp_per_haul$n_spp/10,
+                             bg = spp_per_haul$col,
+                             col = spp_per_haul$col),
+           xlim = c(-11, -5),
+)
+arrows(-10.5, y0 = 56, y1 = 56.8, length = 0.1, lwd = 2)
+raster::scalebar(d = 100,
+                 label = "100 km",
+                 xy = c(-8, 53.1),
+                 lonlat = TRUE)
+## Add colour bar
+TeachingDemos::subplot(add_colour_bar(data.frame(x = spp_per_haul_col_param$breaks,
+                                                 col = c(spp_per_haul_col_param$col, NA)),
+                                      pretty_axis_args = spp_per_haul_paa,
+                                      mtext_args = list(side = 4, "Species richness", line = 3)
+),
+x = c(-4.8, -4.5), y = c(54, 56))
+## Add titles
+mtext(side = 1, expression("Longitude (" * degree * ")"), cex = 1.25, line = 2)
+mtext(side = 2, expression("Latitude (" * degree * ")"), cex = 1.25, line = -2)
+dev.off()
 
-#### Table of the top 5 species wth the widest distribution
+#### Table of the top 5 species with the widest distribution
 hauls_league_tbl <-
   catches %>%
   dplyr::group_by(Species) %>%
